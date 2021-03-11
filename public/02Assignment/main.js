17f14ec73df25a745bef94582606e6014b17c348
JcJen62 <jcjensen62@gmail.com>
2021-03-10 20:18:37 -0700

    Final Changes

--- public/02Assignment/main.js
+++ public/02Assignment/main.js
@@ -1,8 +1,18 @@
-document.querySelector('#companyName').innerText = "Joe's Bed and Breakfast"
-document.querySelector('#companyMoto').innerText = "Stay Awhile and Relax!"
+document.querySelector("#companyName").innerText = "Inspector Clouseau";
+document.querySelector("#companyMoto").innerText = "I Find all the answers";
 
+function reDisplay() {
+  let windowSizes = `Window size is ${window.innerWidth} pixel wide by ${window.innerHeight} pixels tall.`;
+  let offset = `Window offset is ${window.screenX} from the left edge and ${window.screenY}and pixels from the top of the display.`;
+  let windowURL = `This Windows URL is ${window.location}`;
 
-let todaysDate = new Date();
-document.getElementById('date').innerHTML = `Today is ${todaysDate.toLocaleDateString()} keep this in mind when booking!`;
+  let myWindow = (document.querySelector("#myWindow").innerHTML =
+    windowSizes + "<br>" + offset + "<br>" + windowURL);
+}
 
-document.getElementById('nameVisitor').innerHTML = `Hello ${prompt("Please enter your name")} thanks for visiting`
+reDisplay()
+
+let title = `The document title is: ${document.title}`
+let updated = `The site was last updated: ${document.lastModified}`
+
+let myDocument = document.querySelector("#myDocument").innerHTML = title + "<br>" + updated
